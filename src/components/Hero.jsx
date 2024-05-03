607191daa49af2303c411787dbc0889c8aa0402a
piyush960 <piyushdahake096@gmail.com>
2024-05-03 20:36:12 +0530

    hero section update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className='flex bg-blue-100 justify-center items-center rounded-md p-6 w-[1270px]  m-10 max-md:flex-col-reverse'>
         <div className='w-[50%]'>
           <h2 className='text-4xl font-bold drop-shadow-sm'>Find the best Hospitals in Emergency</h2>
-          <p className='text-slate-700 py-9 font-normal w-[80%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex eos cum consectetur, suscipit consequatur nisi rem alias impedit officia dolorum.</p>
+          <p className='text-slate-700 py-9 font-normal w-[80%]'>Give it a try!</p>
           <Link to={'/enter-symptoms'} className='btn-primary'>Find Hospital</Link>
         </div>
         <img src="/assets/images/hospital.svg" alt="doctor-image" className='w-[350px] h-[350px]'/>
